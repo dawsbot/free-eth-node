e41e999778b46b03f8fcdf21d4adb7e195782e3a
daws.eth <dawsonbotsford@gmail.com>
2022-04-04 10:40:40 -0700

    Add github corner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import GithubCorner from "react-github-corner";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GithubCorner
+        href="https://github.com/earnifi/free-eth-node"
+        size={200}
+        bannerColor={"#27ae60"}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
