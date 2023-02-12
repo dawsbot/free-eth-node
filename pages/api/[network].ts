4707b5671ed3676eb48a8062bebcb7c3d9a5d9b7
dawsbot <dawsonbotsford@gmail.com>
2023-02-11 21:59:02 -0700

    🐛 Fix mainnet
    * Also remove cache

--- pages/api/[network].ts
+++ pages/api/[network].ts
@@ -1,18 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
-import NodeCache from "node-cache";
 import chains from "../../chains.min.json";
-
-const ipAddressDailyLimit = 10000;
 
 type Data = {
   error: string;
 };
-const myCache = new NodeCache({
-  stdTTL: 60 * 60 * 24 /* one day */,
-  checkperiod: 30 /* check every 30 seconds */,
-});
 
 const isDev = process.env.NODE_ENV !== "production";
 const allShortNames = chains.map((chain) => chain.shortName);
@@ -25,13 +18,6 @@ export default async function handler(
     req.socket.remoteAddress ||
     "missing"
   ).toString();
-  const usedTimes = myCache.get(ipAddress);
-  if (typeof usedTimes === "number" && usedTimes > ipAddressDailyLimit) {
-    return res.status(400).json({
-      error:
-        "You've reached your limit on free-eth-handler. Come back in a day",
-    });
-  }
   const network = req.query.network as string;
   if (!allShortNames.includes(network)) {
     return res.status(400).json({
@@ -46,9 +32,6 @@ export default async function handler(
       params1: req.body.params?.[0],
       params2: req.body.params?.[1],
     });
-  } else {
-    myCache.set(ipAddress, typeof usedTimes === "number" ? usedTimes + 1 : 1);
-    console.log({ ipAddress, usedTimes });
   }
   const rpc = (
     chains.find((chain) => chain.shortName === network) || { rpc: "bad" }
