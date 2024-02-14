469323781b9b912dce8fd5ab5027c199f5c633a9
dawsbot <dawsonbotsford@gmail.com>
2024-02-14 11:11:45 -0700

    Update deps

--- pages/api/[network].ts
+++ pages/api/[network].ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch";
 import chains from "../../chains.min.json";
 
 type Data = {
